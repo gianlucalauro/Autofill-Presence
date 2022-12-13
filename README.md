b64411e71b649fccbaa41901a4d856a1b06c3bee
gianlucalauro <giangi.lauro@gmail.com>
2022-12-13 09:03:06 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Autofill Presence
+
+## Instructions
+
+0. cd into project directory
+1. install dependencies with: `pip install -r requirements.txt` 
+2. insert your credentials in: *credentials.json*
+
+> When you are sure that the script works correctly, you can also start a cronjob
---
+++ autofill_presence.py
@@ -0,0 +1,55 @@
+import sys
+import json
+import os
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+
+
+def do(by, element, event, string=None):
+    wait_event = wait.until(EC.visibility_of_element_located((by, element)))
+    if event == "send_keys":
+        wait_event.send_keys(string)
+    if event == "click":
+        wait_event.click()
+    if event == "clear_and_send_keys":
+        wait_event.clear()
+        wait_event.send_keys(string)
+
+def login():
+    __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+    credentials = json.load(open(os.path.join(__location__, 'credentials.json')))
+    do(By.NAME, "email", "send_keys", credentials["email"])
+    do(By.NAME, "password", "send_keys", credentials["password"])
+    do(By.ID, "kt_sign_in_submit", "click")
+    do(By.XPATH, "//*[contains(text(), 'Ok, got it!')]", "click")
+
+
+def insert_asa_and_submit():
+    do(By.XPATH, "//*[contains(text(), 'Aggiungi Presenza')]", "click")
+    for i in range(2, len(sys.argv)):
+        do(By.NAME, sys.argv[i].split('=')[0], "clear_and_send_keys", sys.argv[i].split('=')[1])
+    do(By.XPATH, "//form[@id='kt_modal_add_schedule_form']/div[3]/span/span/span", "click")
+    do(By.XPATH, f"//li[text()='{sys.argv[1]}']", "click")
+    do(By.XPATH, "//button[text()='Salva Presenza']", "click")
+
+
+options = Options()
+options.headless = True
+
+driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
+
+driver.get("https://example.com")
+
+wait = WebDriverWait(driver, 20)
+
+login()
+
+driver.get("https://example.com/profile")
+
+insert_asa_and_submit()
+
+driver.close()
